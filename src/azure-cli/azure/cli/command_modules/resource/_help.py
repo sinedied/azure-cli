a2048a2f0a7dee18f1b068370cc594389e621b56
Yohan Lasorsa <noda@free.fr>
2021-09-14 10:20:36 +0200

    docs: remove misleading example

--- src/azure-cli/azure/cli/command_modules/resource/_help.py
+++ src/azure-cli/azure/cli/command_modules/resource/_help.py
@@ -625,9 +625,6 @@ examples:
         az deployment group create --resource-group testrg --name rollout01 \\
             --template-file azuredeploy.json  --parameters @params.json \\
             --parameters https://mysite/params.json --parameters MyValue=This MyArray=@array.json
-  - name: Create a deployment at subscription scope from a template-spec
-    text: >
-        az deployment group create --resource-group testrg --template-spec "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testrg/providers/Microsoft.Resources/templateSpecs/myTemplateSpec/versions/1.0"
 """
 
 helps['deployment group what-if'] = """
